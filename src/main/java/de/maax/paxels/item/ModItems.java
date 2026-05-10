9da6db74e525170ed7ba66b5aa66d0e0ac7c14e3
maaaax0 <meyermax.731@gmail.com>
2026-05-10 15:55:29 +0200

    Set item id during paxel registration

--- src/main/java/de/maax/paxels/item/ModItems.java
+++ src/main/java/de/maax/paxels/item/ModItems.java
@@ -34,12 +34,12 @@ public class ModItems {
             registerPaxel("netherite_paxel", ToolMaterial.NETHERITE, 6.0F, -2.8F);
 
     private static DeferredItem<Item> registerPaxel(String name, ToolMaterial material, float attackDamageModifier, float attackSpeedModifier) {
-        return ITEMS.register(name,
-                () -> new PaxelItem(
+        return ITEMS.registerItem(name,
+                properties -> new PaxelItem(
                         material,
                         attackDamageModifier,
                         attackSpeedModifier,
-                        new Item.Properties()
+                        properties
                 )
         );
     }
