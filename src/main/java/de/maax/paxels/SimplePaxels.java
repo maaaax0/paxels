1d469d31c1b6d281c4a8d8fd878f9a93551aac42
maaaax0 <meyermax.731@gmail.com>
2026-05-10 22:25:29 +0200

    Port paxels to Fabric 1.21.10

--- src/main/java/de/maax/paxels/SimplePaxels.java
+++ src/main/java/de/maax/paxels/SimplePaxels.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -39,7 +39,7 @@ public class SimplePaxels implements ModInitializer {
         );
     }
 
-    public static Identifier id(String path) {
-        return Identifier.fromNamespaceAndPath(MODID, path);
+    public static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 }
